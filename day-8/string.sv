d40e9192118fbc4786dd97ffa0f4bb74640e6ff1
Janarthanan-14 <janarthanannraj@gmail.com>
2025-03-13 19:28:27 +0530

    Create string.sv

---
+++ day-8/string.sv
@@ -0,0 +1,16 @@
+module tb;
+	
+	string   fruits[$] =  { "orange", "apple", "lemon", "kiwi" };
+
+	initial begin
+		
+		$display ("citrus fruits = %p", fruits[1:2]);
+
+		$display ("fruits = %p", fruits[1:$]);
+
+		fruits[$+1] = "pineapple";
+		$display ("fruits = %p", fruits);
+
+		$display ("Remove orange, fruits = %p", fruits[1:$]);
+	end
+endmodule
